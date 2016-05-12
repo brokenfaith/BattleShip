3d00b9461d5834373f2066fec09f1b566edc7413
BrianBartow <zerorage7@gmail.com>
2016-05-11 18:44:52 -0700

    files to start with

---
+++ src/main/java/Player.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class Player {
+	// Grid on which the player places ships
+	Grid myGrid;
+	
+	public Player(Grid grid) {
+		myGrid = grid;
+	}
+	
+	public Grid getGrid() {
+		return myGrid;
+	}
+	
+	public boolean hasShips() {
+		return !myGrid.gameOver();
+	}
+}
+
---
+++ src/main/java/Utilities.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Utilities {
+	public static boolean between(double a, double b, double c) {
+		return (a >= b) && (a <= c);
+	}
+}
+
